e7550021eb0a63855cb67c5beb64fe1ce94345d4
praneeth <gp.gunturu@gmail.com>
2025-06-09 18:19:04 -0500

    dynamic routing. Its same as with the app router

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,11 @@
+import Link from "next/link";
+
 export default function NewsPage() {
   return (
-    <h1>News</h1>
+    <>
+      <h1>News</h1>
+      <Link href='/news/page1'>Link 1</Link>
+      <Link href='/news/page2'>Link 2</Link>
+    </>
   )
 }
